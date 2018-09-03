c6c7c19e62458f2149adbaf4be0b7b73f63948b1
ktamilara <42666475+ktamilara@users.noreply.github.com>
2018-09-03 13:45:01 +0530

    Create ListEvenNum.java

---
+++ ListEvenNum.java
@@ -0,0 +1,18 @@
+public class ListEvenNum {
+ 
+	public static void main(String[] args) {
+		
+		
+		int limit = 6 ,9;
+		
+		System.out.println("Printing Even numbers between 1 and " + limit);
+		
+		for(int i=1; i <= limit; i++){
+			
+			
+			if( i % 2 == 0){
+				System.out.print(i + " ");
+			}
+		}
+	}
+}
