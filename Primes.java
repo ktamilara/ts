b53fc6be7c77dfbfb5e6470fe08e649033eae42a
ktamilara <42666475+ktamilara@users.noreply.github.com>
2018-09-03 13:48:45 +0530

    Create Primes.java

---
+++ Primes.java
@@ -0,0 +1,24 @@
+public class Primes {
+
+    public static void main(String[] args) {
+
+        int low = 1, high = 10;
+
+        while (low < high) {
+            boolean flag = false;
+
+            for(int i = 2; i <= low/2; ++i) {
+               
+                if(low % i == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag)
+                System.out.print(low + " ");
+
+            ++low;
+        }
+    }
+}
